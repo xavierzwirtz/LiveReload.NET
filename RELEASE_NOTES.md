d538663ce6e13580a7defb2400abf92ab323bd69
Xavier Zwirtz <xavier@birddogsw.com>
2015-04-27 17:20:01 -0500

    Bump version to 1.0.3

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+### 1.0.3 - Released
+* Removed FSharp-ism from public api.
+* Removed debug code.
 ### 1.0.2 - Released
 * Initial release
--- src/LiveReload.NET.Example/AssemblyInfo.fs
+++ src/LiveReload.NET.Example/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("LiveReload.NET.Example")>]
 [<assembly: AssemblyProductAttribute("LiveReload.NET")>]
 [<assembly: AssemblyDescriptionAttribute("Implements a LiveReload server for .NET")>]
-[<assembly: AssemblyVersionAttribute("1.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.2")>]
+[<assembly: AssemblyVersionAttribute("1.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.2"
+    let [<Literal>] Version = "1.0.3"
